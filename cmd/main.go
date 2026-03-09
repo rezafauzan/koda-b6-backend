eb557d6c5fedf5aa8e2fb666acada22679de1952
rezafauzan <rezafauzan593@gmail.com>
2026-03-09 16:25:13 +0700

    Feat: add UsersRouters on main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"rezafauzan/koda-b6-golang/internal/middleware"
+	"rezafauzan/koda-b6-golang/internal/routers"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 
+	router := gin.Default()
+
+	router.Use(middleware.CORSMiddleware())
+
+	routers.NewUserRouters(router)
+
+	router.Run(fmt.Sprintf("localhost:%s", os.Getenv("PORT")))
 }
