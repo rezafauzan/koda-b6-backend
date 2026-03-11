cc668caec7a0e0f59f54027e34e9d3c158d4b6d5
rezafauzan <rezafauzan593@gmail.com>
2026-03-11 10:33:57 +0700

    Feat: add ForgotPasswordRepo GetDataByEmailCode Method

--- internal/repository/forgot_password.repository.go
+++ internal/repository/forgot_password.repository.go
@@ -1,7 +1,9 @@
 package repository
 
 import (
+	"context"
 	"rezafauzan/koda-b6-golang/internal/lib"
+	"rezafauzan/koda-b6-golang/internal/models"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -10,7 +12,7 @@ type ForgotPasswordRepository struct {
 	db *pgx.Conn
 }
 
-func NewForgotPasswordRepository() (*ForgotPasswordRepository, error){
+func NewForgotPasswordRepository() (*ForgotPasswordRepository, error) {
 	db, err := lib.DatabaseConnect()
 	if err != nil {
 		return nil, err
@@ -19,4 +21,17 @@ func NewForgotPasswordRepository() (*ForgotPasswordRepository, error){
 	return &ForgotPasswordRepository{
 		db: db,
 	}, nil
+}
+
+func (f ForgotPasswordRepository) GetDataByEmailCode(email string) (models.ForgotPassword, error) {
+	sql := "SELECT email, code_otp, expired_at, created_at, updated_at FROM forgot_password WHERE email = $1"
+	rows, err := f.db.Query(context.Background(), sql, email)
+	if err != nil {
+		return models.ForgotPassword{}, err
+	}
+	data, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.ForgotPassword])
+	if err != nil {
+		return models.ForgotPassword{}, err
+	}
+	return data, nil
 }
