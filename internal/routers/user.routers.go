d81f75329e316eb8fbd2ee9506ddecb9cc0c1354
rezafauzan <rezafauzan593@gmail.com>
2026-03-12 09:20:18 +0700

    Feat: now route GET /users using GetAllUsers handler

--- internal/routers/user.routers.go
+++ internal/routers/user.routers.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"rezafauzan/koda-b6-golang/internal/dto"
+	"rezafauzan/koda-b6-golang/internal/handlers"
 	"rezafauzan/koda-b6-golang/internal/lib"
 	"rezafauzan/koda-b6-golang/internal/models"
 	"strings"
@@ -16,44 +17,7 @@ import (
 func NewUserRouters(router *gin.Engine) {
 	userRoutes := router.Group("/users")
 	{
-		userRoutes.GET("", func(ctx *gin.Context) {
-			conn, err := lib.DatabaseConnect()
-			if err != nil {
-				ctx.JSON(http.StatusOK, dto.Response{
-					Success:  false,
-					Messages: "Failed to connect to database! : " + err.Error(),
-					Results:  nil,
-				})
-				return
-			}
-
-			sql := `SELECT users.id, user_profiles.user_avatar, user_profiles.first_name, user_profiles.last_name, user_credentials.email, user_credentials.phone, user_profiles.address, users.verified, roles.role_name, users.created_at, users.updated_at FROM users JOIN roles ON roles.id = users.role_id JOIN user_profiles ON user_profiles.user_id = users.id JOIN user_credentials ON user_credentials.user_id = users.id`
-			rows, err := conn.Query(context.Background(), sql)
-			if err != nil {
-				ctx.JSON(http.StatusOK, dto.Response{
-					Success:  false,
-					Messages: "Failed to get all users! : " + err.Error(),
-					Results:  nil,
-				})
-				return
-			}
-
-			users, err := pgx.CollectRows(rows, pgx.RowToStructByName[dto.User])
-			if err != nil {
-				ctx.JSON(http.StatusOK, dto.Response{
-					Success:  false,
-					Messages: "Failed to create response get all users! : " + err.Error(),
-					Results:  nil,
-				})
-				return
-			}
-
-			ctx.JSON(http.StatusOK, dto.Response{
-				Success:  true,
-				Messages: "GET users",
-				Results:  users,
-			})
-		})
+		userRoutes.GET("", handlers.GetAllUsers)
 
 		userRoutes.POST("", func(ctx *gin.Context) {
 			var newUser dto.UserRegister
