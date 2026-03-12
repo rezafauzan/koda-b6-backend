05133311d4ff377e255b41bf0f3f27e0e6656f74
rezafauzan <rezafauzan593@gmail.com>
2026-03-12 09:19:29 +0700

    Feat: splitting route handler from GET /users

---
+++ internal/handlers/user.handlers.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+	"rezafauzan/koda-b6-golang/internal/dto"
+	"rezafauzan/koda-b6-golang/internal/lib"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+)
+
+func GetAllUsers(ctx *gin.Context) {
+	conn, err := lib.DatabaseConnect()
+	if err != nil {
+		ctx.JSON(http.StatusOK, dto.Response{
+			Success:  false,
+			Messages: "Failed to connect to database! : " + err.Error(),
+			Results:  nil,
+		})
+		return
+	}
+
+	sql := `SELECT users.id, user_profiles.user_avatar, user_profiles.first_name, user_profiles.last_name, user_credentials.email, user_credentials.phone, user_profiles.address, users.verified, roles.role_name, users.created_at, users.updated_at FROM users JOIN roles ON roles.id = users.role_id JOIN user_profiles ON user_profiles.user_id = users.id JOIN user_credentials ON user_credentials.user_id = users.id`
+	rows, err := conn.Query(context.Background(), sql)
+	if err != nil {
+		ctx.JSON(http.StatusOK, dto.Response{
+			Success:  false,
+			Messages: "Failed to get all users! : " + err.Error(),
+			Results:  nil,
+		})
+		return
+	}
+
+	users, err := pgx.CollectRows(rows, pgx.RowToStructByName[dto.User])
+	if err != nil {
+		ctx.JSON(http.StatusOK, dto.Response{
+			Success:  false,
+			Messages: "Failed to create response get all users! : " + err.Error(),
+			Results:  nil,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, dto.Response{
+		Success:  true,
+		Messages: "GET users",
+		Results:  users,
+	})
+}
