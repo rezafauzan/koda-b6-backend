ccb625fe15d4aa5d9e9d0356db0d6e6d09e6a2c2
rezafauzan <rezafauzan593@gmail.com>
2026-03-14 22:37:22 +0700

    Feat: user handler now call user service only and return json

--- internal/handlers/user.handlers.go
+++ internal/handlers/user.handlers.go
@@ -5,15 +5,17 @@ import (
 	"net/http"
 	"rezafauzan/koda-b6-golang/internal/dto"
 	"rezafauzan/koda-b6-golang/internal/lib"
-	"rezafauzan/koda-b6-golang/internal/models"
-	"strings"
-	"time"
+	"rezafauzan/koda-b6-golang/internal/services"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 )
 
-func GetAllUsers(ctx *gin.Context) {
+type UserHandler struct {
+	userService *services.UserService
+}
+
+func (u UserHandler) GetAllUsers(ctx *gin.Context) {
 	conn, err := lib.DatabaseConnect()
 	if err != nil {
 		ctx.JSON(http.StatusOK, dto.Response{
@@ -52,124 +54,21 @@ func GetAllUsers(ctx *gin.Context) {
 	})
 }
 
-func AddNewUser(ctx *gin.Context) {
+func (u UserHandler) AddNewUser(ctx *gin.Context) {
 	var newUser dto.UserRegister
 	ctx.ShouldBind(&newUser)
-	if len(newUser.First_name) < 4 {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create user! : First name length minimum is 4 characters !",
-			Results:  nil,
-		})
-		return
-	}
-	if len(newUser.Last_name) < 4 {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create user! : Last name length minimum is 4 characters !",
-			Results:  nil,
-		})
-		return
-	}
-	if !strings.Contains(newUser.Email, "@") {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create user! : Invalid email format !",
-			Results:  nil,
-		})
-		return
-	}
-	if len(newUser.Phone) < 10 {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create user! : Phone numbers length minimum 10 digits !",
-			Results:  nil,
-		})
-		return
-	}
-	if len(newUser.Address) < 10 {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create user! : Address length minimum is 10 characters !",
-			Results:  nil,
-		})
-		return
-	}
-	if len(newUser.Password) < 8 {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create user! : Password too weak minimum length is 8 characters !",
-			Results:  nil,
-		})
-		return
-	}
-	if newUser.Password_confirm != newUser.Password {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create user! : Password confirmation missmatch !",
-			Results:  nil,
-		})
-		return
-	}
-	conn, err := lib.DatabaseConnect()
+	newUser, err := u.userService.AddNewUser(newUser)
 	if err != nil {
 		ctx.JSON(http.StatusOK, dto.Response{
 			Success:  false,
-			Messages: "Failed to connect to database! : " + err.Error(),
+			Messages: err.Error(),
 			Results:  nil,
 		})
 		return
 	}
-	sql := "INSERT INTO users (role_id,verified,created_at,updated_at) VALUES ($1, $2, $3, $4) RETURNING id, role_id, verified, created_at,updated_at"
-	rows, err := conn.Query(context.Background(), sql, 2, false, time.Now(), time.Now())
-	if err != nil {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create new user! : " + err.Error(),
-			Results:  nil,
-		})
-		return
-	}
-
-	registeredUser, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.User])
-	if err != nil {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create new user! : " + err.Error(),
-			Results:  nil,
-		})
-		return
-	}
-
-	sql = "INSERT INTO user_profiles (user_id, user_avatar, first_name, last_name, address, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7)"
-	commandTag, err := conn.Exec(context.Background(), sql, registeredUser.Id, "https://i.pravatar.cc/400?img=4", newUser.First_name, newUser.Last_name, newUser.Address, time.Now(), time.Now())
-
-	if err != nil {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create new user! : " + err.Error(),
-			Results:  nil,
-		})
-		return
-	}
-
-	sql = "INSERT INTO user_credentials (user_id, email, phone, password, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)"
-	commandTag, err = conn.Exec(context.Background(), sql, registeredUser.Id, newUser.Email, newUser.Phone, newUser.Password, time.Now(), time.Now())
-
-	if err != nil {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  false,
-			Messages: "Failed to create new user! : " + err.Error(),
-			Results:  nil,
-		})
-		return
-	}
-
-	if commandTag.RowsAffected() > 0 {
-		ctx.JSON(http.StatusOK, dto.Response{
-			Success:  true,
-			Messages: "New users successfully",
-			Results:  nil,
-		})
-	}
+	ctx.JSON(http.StatusOK, dto.Response{
+		Success:  false,
+		Messages: "Register Success!",
+		Results:  newUser,
+	})
 }
