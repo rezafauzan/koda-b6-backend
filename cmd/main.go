49fd678337bfd15a2ca0bfabf4077f457f084372
rezafauzan <rezafauzan593@gmail.com>
2026-03-16 05:41:47 +0700

    Feat: call container in main

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"rezafauzan/koda-b6-golang/internal/di"
 	"rezafauzan/koda-b6-golang/internal/middleware"
 	"rezafauzan/koda-b6-golang/internal/routers"
 
@@ -11,13 +12,19 @@ import (
 )
 
 func main() {
+	defer recover()
 	godotenv.Load()
 
 	router := gin.Default()
+	container, err := di.NewContainer()
+
+	if err != nil {
+		panic("Container Error")
+	}
 
 	router.Use(middleware.CORSMiddleware())
 
-	routers.NewUserRouters(router)
+	routers.NewUserRouters(router, container)
 
 	router.Run(fmt.Sprintf("localhost:%s", os.Getenv("PORT")))
 }
