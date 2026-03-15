f6baa6f5647774fa85bdf8298998f887e4361183
rezafauzan <rezafauzan593@gmail.com>
2026-03-15 12:54:19 +0700

    Feat: now dependencies injected from di.container

---
+++ internal/di/container.go
@@ -0,0 +1,31 @@
+package di
+
+import (
+	"rezafauzan/koda-b6-golang/internal/handlers"
+	"rezafauzan/koda-b6-golang/internal/lib"
+	"rezafauzan/koda-b6-golang/internal/repository"
+	"rezafauzan/koda-b6-golang/internal/services"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type Container struct {
+	db *pgx.Conn
+	UserHandler *handlers.UserHandler
+}
+
+func NewContainer() (*Container, error){
+	db, err := lib.DatabaseConnect()
+	if err != nil {
+		return nil, err
+	}
+	return &Container{
+		db: db,
+	}, nil
+}
+
+func (c *Container) initDependencies(){
+	userRepo , _ := repository.NewUserRepository(c.db)
+	userService := services.NewUserService(userRepo)
+	c.UserHandler = handlers.NewUserHandler(userService)
+}
