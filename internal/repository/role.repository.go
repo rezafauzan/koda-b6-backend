a802a371a169a98daacbfd1bc9d47d2b97c21380
rezafauzan <rezafauzan593@gmail.com>
2026-03-16 10:28:36 +0700

    Feat: add repository.role.GetAllRoles and repository.role.GetRoleById

--- internal/repository/role.repository.go
+++ internal/repository/role.repository.go
@@ -18,6 +18,7 @@ func NewRoleRepository(db *pgx.Conn) (*RoleRepository, error) {
 		db: db,
 	}, nil
 }
+
 func (u RoleRepository) AddNewRole(newRole models.Role) (models.Role, error) {
 	sql := "INSERT INTO roles (role_name, created_at, updated_at) VALUES ($1, $2, $3) RETURNING id, role_name, created_at, updated_at"
 	rows, err := u.db.Query(context.Background(), sql, newRole.Role_name, time.Now(), time.Now())
@@ -31,4 +32,34 @@ func (u RoleRepository) AddNewRole(newRole models.Role) (models.Role, error) {
 	}
 
 	return registeredRole, nil
+}
+
+func (u RoleRepository) GetAllRoles() ([]models.Role, error) {
+	sql := `SELECT id, role_name, created_at, updated_at FROM roles`
+	rows, err := u.db.Query(context.Background(), sql)
+	if err != nil {
+		return []models.Role{}, err
+	}
+
+	roles, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Role])
+	if err != nil {
+		return []models.Role{}, errors.New("Failed to create response get all roles! : " + err.Error())
+	}
+
+	return roles, nil
+}
+
+func (u RoleRepository) GetRoleById(id int) (models.Role, error) {
+	sql := `SELECT id, role_name, created_at, updated_at FROM roles WHERE id = $1`
+	rows, err := u.db.Query(context.Background(), sql, id)
+	if err != nil {
+		return models.Role{}, err
+	}
+
+	role, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[models.Role])
+	if err != nil {
+		return models.Role{}, err
+	}
+
+	return role, nil
 }
