5fba615a3aa1d9dc3c022068f67ce910e9fabb48
rezafauzan <rezafauzan593@gmail.com>
2026-03-16 16:58:40 +0700

    Feat: add retry connect db

--- internal/lib/database_connect.go
+++ internal/lib/database_connect.go
@@ -5,15 +5,27 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func DatabaseConnect() (*pgx.Conn, error) {
-	connection, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		return nil, errors.New("Could not connect to database!")
+	dbURL := os.Getenv("DATABASE_URL")
+
+	var connection *pgx.Conn
+	var err error
+
+	for i := 0; i < 10; i++ {
+		connection, err = pgx.Connect(context.Background(), dbURL)
+		if err == nil {
+			fmt.Println("Connection to database established!")
+			return connection, nil
+		}
+
+		fmt.Println("Database not ready, retrying...")
+		time.Sleep(4 * time.Second)
 	}
-	fmt.Println("Connection to database established!")
-	return connection, nil
+
+	return nil, errors.New("could not connect to database : " + err.Error())
 }
