f902b64c08418392a58ec361a1c9dc3608abbe7f
rezafauzan <rezafauzan593@gmail.com>
2026-03-16 11:38:53 +0700

    Fix: typo role_id not found

--- internal/repository/role.repository.go
+++ internal/repository/role.repository.go
@@ -19,7 +19,7 @@ func NewRoleRepository(db *pgx.Conn) (*RoleRepository, error) {
 	}, nil
 }
 
-func (u RoleRepository) AddNewRole(newRole models.Role) (models.Role, error) {
+func (u RoleRepository) AddNewRole(newRole *models.Role) (models.Role, error) {
 	sql := "INSERT INTO roles (role_name, created_at, updated_at) VALUES ($1, $2, $3) RETURNING id, role_name, created_at, updated_at"
 	rows, err := u.db.Query(context.Background(), sql, newRole.Role_name, time.Now(), time.Now())
 	if err != nil {
@@ -74,7 +74,7 @@ func (u RoleRepository) UpdateRole(newData models.Role) (models.Role, error) {
 		newData.Role_name = role.Role_name
 	}
 
-	sql := `UPDATE roles SET role_name = $1, updated_at = $2 WHERE role_id = $3`
+	sql := `UPDATE roles SET role_name = $1, updated_at = $2 WHERE id = $3`
 
 	_, err = u.db.Exec(context.Background(), sql, newData.Role_name, time.Now(), newData.Id)
 	if err != nil {
