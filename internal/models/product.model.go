4db0906f7a7ae3cb43e226270701180e0b0dd773
rezafauzan <rezafauzan593@gmail.com>
2026-03-17 09:19:26 +0700

    Feat: add product-images model

--- internal/models/product.model.go
+++ internal/models/product.model.go
@@ -3,16 +3,16 @@ package models
 import "time"
 
 type Product struct {
-	ID          int       `json:"id"`
-	CategoryID  int       `json:"category_id"`
+	Id          int       `json:"id"`
+	Category_id int       `json:"category_id"`
 	Favorite    bool      `json:"favorite_product"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
 	Price       int       `json:"price"`
-	CampaignID  int       `json:"campaign_id"`
+	Campaign_id int       `json:"campaign_id"`
 	Stock       int       `json:"stock"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	Created_at  time.Time `json:"created_at"`
+	Updated_at  time.Time `json:"updated_at"`
 	Images      []string  `json:"images"`
 	Variants    []string  `json:"variants"`
 	Portions    []string  `json:"portions"`
