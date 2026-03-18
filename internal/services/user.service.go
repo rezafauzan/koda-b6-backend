b4d147f16de21626badf07a20f24cc0af9027323
rezafauzan <rezafauzan593@gmail.com>
2026-03-18 12:49:48 +0700

    Feat: add email or phone exist validation on registration

--- internal/services/user.service.go
+++ internal/services/user.service.go
@@ -17,29 +17,42 @@ func NewUserService(userRepo *repository.UserRepository) *UserService {
 	}
 }
 
-func (u UserService) AddNewUser(newUser *dto.CreateUserDTO) (*dto.CreateUserDTO, error) {
+func (u UserService) CreateNewUser(newUser dto.CreateUserDTO) (dto.CreateUserDTO, error) {
+	
 	if len(newUser.First_name) < 4 {
-		return &dto.CreateUserDTO{}, errors.New("Failed to create user! : First name length minimum is 4 characters !")
+		return dto.CreateUserDTO{}, errors.New("Failed to create user! : First name length minimum is 4 characters !")
 	}
 	if len(newUser.Last_name) < 4 {
-		return &dto.CreateUserDTO{}, errors.New("Failed to create user! : Last name length minimum is 4 characters !")
+		return dto.CreateUserDTO{}, errors.New("Failed to create user! : Last name length minimum is 4 characters !")
 	}
 	if !strings.Contains(newUser.Email, "@") {
-		return &dto.CreateUserDTO{}, errors.New("Failed to create user! : Invalid email format !")
+		return dto.CreateUserDTO{}, errors.New("Failed to create user! : Invalid email format !")
 	}
 	if len(newUser.Phone) < 10 {
-		return &dto.CreateUserDTO{}, errors.New("Failed to create user! : Phone numbers length minimum 10 digits !")
+		return dto.CreateUserDTO{}, errors.New("Failed to create user! : Phone numbers length minimum 10 digits !")
 	}
 	if len(newUser.Address) < 10 {
-		return &dto.CreateUserDTO{}, errors.New("Failed to create user! : Address length minimum is 10 characters !")
+		return dto.CreateUserDTO{}, errors.New("Failed to create user! : Address length minimum is 10 characters !")
 	}
 	if len(newUser.Password) < 8 {
-		return &dto.CreateUserDTO{}, errors.New("Failed to create user! : Password too weak minimum length is 8 characters !")
+		return dto.CreateUserDTO{}, errors.New("Failed to create user! : Password too weak minimum length is 8 characters !")
 	}
 	if newUser.Password_confirm != newUser.Password {
-		return &dto.CreateUserDTO{}, errors.New("Failed to create user! : Password confirmation missmatch !")
+		return dto.CreateUserDTO{}, errors.New("Failed to create user! : Password confirmation missmatch !")
 	}
-	return u.userRepo.AddNewUser(newUser)
+	_, emailExist, _ := u.userRepo.GetUserByEmail(newUser.Email)
+
+	if emailExist {
+		return dto.CreateUserDTO{}, errors.New("Failed to create new user! : Email allready used !")
+	}
+
+	_, phoneExist, _ := u.userRepo.GetUserByPhone(newUser.Phone)
+
+	if phoneExist {
+		return dto.CreateUserDTO{}, errors.New("Failed to create new user! : Phone number allready used !")
+	}
+	
+	return u.userRepo.CreateNewUser(newUser)
 }
 
 func (u UserService) GetAllUser() ([]dto.UserResponseDTO, error) {
