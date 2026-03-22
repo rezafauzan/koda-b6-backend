8b52419b5cf75024d2d444f4fb4774de86173b51
rezafauzan <rezafauzan593@gmail.com>
2026-03-22 10:28:46 +0700

    Feat: add forgot password routers for request and reset password

---
+++ internal/routers/forgot_password.routers.go
@@ -0,0 +1,20 @@
+package routers
+
+import (
+	"rezafauzan/koda-b6-golang/internal/di"
+	"rezafauzan/koda-b6-golang/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ForgotPasswordRouter struct {
+	forgotPasswordHandler *handlers.ForgotPasswordHandler
+}
+
+func NewForgotPasswordRouters(router *gin.Engine, container *di.Container) {
+	forgotPasswordRoutes := router.Group("/forgot-password")
+	{
+		forgotPasswordRoutes.POST("request", container.ForgotPasswordHandler.RequestForgotPassword)
+		forgotPasswordRoutes.POST("reset", container.ForgotPasswordHandler.ResetPassword)
+	}
+}
