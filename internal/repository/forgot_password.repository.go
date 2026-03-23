0a32a677ba41e6cb88c2c28aa55b2d305ed45d8d
rezafauzan <rezafauzan593@gmail.com>
2026-03-23 17:11:44 +0700

    Feat: add forgot password repository metod to create forgot password record and reset password

--- internal/repository/forgot_password.repository.go
+++ internal/repository/forgot_password.repository.go
@@ -2,8 +2,7 @@ package repository
 
 import (
 	"context"
-	"math/big"
-	"rezafauzan/koda-b6-golang/internal/lib"
+	"errors"
 	"rezafauzan/koda-b6-golang/internal/models"
 	"time"
 
@@ -14,52 +13,138 @@ type ForgotPasswordRepository struct {
 	db *pgx.Conn
 }
 
-func NewForgotPasswordRepository() (*ForgotPasswordRepository, error) {
-	db, err := lib.DatabaseConnect()
-	if err != nil {
-		return nil, err
-	}
-
+func NewForgotPasswordRepository(db *pgx.Conn) (*ForgotPasswordRepository, error) {
 	return &ForgotPasswordRepository{
 		db: db,
 	}, nil
 }
 
-func (f ForgotPasswordRepository) CreateForgotPasswordData(email string, code_otp *big.Int) (models.ForgotPassword, error) {
-	sql := "INSERT INTO forgot_password (email, code_otp, expired_at) VALUES ($1, $2, $3)"
-	rows, err := f.db.Query(context.Background(), sql, email, code_otp, time.Now().Add(5 * time.Minute))
+func (u ForgotPasswordRepository) CreateForgotPassword(newForgotPassword *models.ForgotPassword) (models.ForgotPassword, error) {
+	sql := "INSERT INTO forgot_password (email, code_otp, created_at, updated_at) VALUES ($1, $2, $3, $4) RETURNING id, email, code_otp, created_at, updated_at"
+	rows, err := u.db.Query(context.Background(), sql, newForgotPassword.Email, newForgotPassword.CodeOtp, time.Now(), time.Now())
+	if err != nil {
+		return models.ForgotPassword{}, errors.New("Failed to create new forgot password! : " + err.Error())
+	}
+
+	registeredForgotPassword, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.ForgotPassword])
+	if err != nil {
+		return models.ForgotPassword{}, errors.New("Failed to create new forgot password! : " + err.Error())
+	}
+
+	return registeredForgotPassword, nil
+}
+
+func (u ForgotPasswordRepository) GetAllForgotPasswords() ([]models.ForgotPassword, error) {
+	sql := `SELECT id, email, code_otp, created_at, updated_at FROM forgot_password`
+	rows, err := u.db.Query(context.Background(), sql)
+	if err != nil {
+		return []models.ForgotPassword{}, err
+	}
+
+	forgotPasswords, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.ForgotPassword])
+	if err != nil {
+		return []models.ForgotPassword{}, errors.New("Failed to create response get all forgot passwords! : " + err.Error())
+	}
+
+	return forgotPasswords, nil
+}
+
+func (u ForgotPasswordRepository) GetForgotPasswordById(id int) (models.ForgotPassword, error) {
+	sql := `SELECT id, email, code_otp, created_at, updated_at FROM forgot_password WHERE id = $1`
+	rows, err := u.db.Query(context.Background(), sql, id)
 	if err != nil {
 		return models.ForgotPassword{}, err
 	}
+
+	forgotPassword, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[models.ForgotPassword])
+	if err != nil {
+		return models.ForgotPassword{}, err
+	}
+
+	return forgotPassword, nil
+}
+
+func (u ForgotPasswordRepository) UpdateForgotPassword(newData models.ForgotPassword) (models.ForgotPassword, error) {
+	forgotPassword, err := u.GetForgotPasswordById(newData.Id)
+	if err != nil {
+		return models.ForgotPassword{}, err
+	}
+
+	if newData.Email == "" {
+		newData.Email = forgotPassword.Email
+	}
+
+	sql := `UPDATE forgot_password SET email = $1, code_otp = $2, updated_at = $3 WHERE id = $4`
+
+	_, err = u.db.Exec(context.Background(), sql, newData.Email, newData.CodeOtp, time.Now(), newData.Id)
+	if err != nil {
+		return models.ForgotPassword{}, err
+	}
+
+	updatedForgotPassword, err := u.GetForgotPasswordById(newData.Id)
+	if err != nil {
+		return models.ForgotPassword{}, err
+	}
+
+	return updatedForgotPassword, nil
+}
+
+func (u ForgotPasswordRepository) DeleteForgotPassword(id int) error {
+	sql := `DELETE FROM forgot_password WHERE id = $1`
+	_, err := u.db.Exec(context.Background(), sql, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u ForgotPasswordRepository) ClearForgotPassword(email string) error {
+	sql := `DELETE FROM forgot_password WHERE email = $1`
+	_, err := u.db.Exec(context.Background(), sql, email)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u ForgotPasswordRepository) CreateForgotPasswordData(email string, code_otp int) (models.ForgotPassword, error) {
+	sql := "INSERT INTO forgot_password (email, code_otp, created_at, updated_at) VALUES ($1, $2, $3, $4) RETURNING id, email, code_otp, created_at, updated_at"
+	rows, err := u.db.Query(context.Background(), sql, email, code_otp, time.Now(), time.Now())
+	if err != nil {
+		return models.ForgotPassword{}, errors.New("Failed to create new forgot password! : " + err.Error())
+	}
+
 	data, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.ForgotPassword])
 	if err != nil {
-		return models.ForgotPassword{}, err
+		return models.ForgotPassword{}, errors.New("Failed to create new forgot password! : " + err.Error())
 	}
+
 	return data, nil
 }
 
-func (f ForgotPasswordRepository) GetDataByEmailCode(email string, code_otp big.Int) (models.ForgotPassword, error) {
-	sql := "SELECT email, code_otp, expired_at FROM forgot_password WHERE email = $1 AND code_otp = $2"
-	rows, err := f.db.Query(context.Background(), sql, email, code_otp)
+func (u ForgotPasswordRepository) GetLatestOTP(email string) (models.ForgotPassword, error) {
+	sql := "SELECT id, email, code_otp, created_at, updated_at FROM forgot_password WHERE email = $1 ORDER BY created_at DESC, id DESC LIMIT 1"
+	rows, err := u.db.Query(context.Background(), sql, email)
 	if err != nil {
 		return models.ForgotPassword{}, err
 	}
+
 	data, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[models.ForgotPassword])
 	if err != nil {
 		return models.ForgotPassword{}, err
 	}
+
 	return data, nil
 }
 
-func (f ForgotPasswordRepository) DeleteDataByEmailCode(email string, code_otp big.Int) (models.ForgotPassword, error) {
-	sql := "DELETE FROM forgot_password WHERE email = $1 AND code_otp = $2"
-	rows, err := f.db.Query(context.Background(), sql, email, code_otp)
+func (u ForgotPasswordRepository) MarkOTPUsed(id int) error {
+	sql := "DELETE FROM forgot_password WHERE id = $1"
+	_, err := u.db.Exec(context.Background(), sql, id)
 	if err != nil {
-		return models.ForgotPassword{}, err
+		return err
 	}
-	data, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.ForgotPassword])
-	if err != nil {
-		return models.ForgotPassword{}, err
-	}
-	return data, nil
+
+	return nil
 }
