f78b024776177cf272fdfbfce42ad2ae52b12024
rezafauzan <rezafauzan593@gmail.com>
2026-03-24 09:26:19 +0700

    Feat: now database_connect is using pgxpool

--- internal/lib/database_connect.go
+++ internal/lib/database_connect.go
@@ -7,25 +7,29 @@ import (
 	"os"
 	"time"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func DatabaseConnect() (*pgx.Conn, error) {
+func DatabaseConnect() (*pgxpool.Pool, error) {
 	dbURL := os.Getenv("DATABASE_URL")
 
-	var connection *pgx.Conn
+	var pool *pgxpool.Pool
 	var err error
 
 	for i := 0; i < 10; i++ {
-		connection, err = pgx.Connect(context.Background(), dbURL)
+		pool, err = pgxpool.New(context.Background(), dbURL)
 		if err == nil {
-			fmt.Println("Connection to database established!")
-			return connection, nil
+			// cek koneksi beneran
+			err = pool.Ping(context.Background())
+			if err == nil {
+				fmt.Println("Connection to database established!")
+				return pool, nil
+			}
 		}
 
 		fmt.Println("Database not ready, retrying...")
 		time.Sleep(4 * time.Second)
 	}
 
-	return nil, errors.New("could not connect to database : " + err.Error())
+	return nil, errors.New("could not connect to database: " + err.Error())
 }
