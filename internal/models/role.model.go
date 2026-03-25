7d42c6dd5b607b458c052010b906ac02ae334215
rezafauzan <rezafauzan593@gmail.com>
2026-03-25 17:39:49 +0700

    Feat: improve role model

--- internal/models/role.model.go
+++ internal/models/role.model.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Role struct {
-	Id         int       `json:"id"`
-	Role_name  string    `json:"role_name"`
-	Created_at time.Time `json:"created_at"`
-	Updated_at time.Time `json:"updated_at"`
+	Id        int       `json:"id" db:"id"`
+	RoleName  string    `json:"role_name" db:"role_name"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
