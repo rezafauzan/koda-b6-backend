b70854cbb3d8db7b7cda1ddbaa4c9d95a6ce0aec
rezafauzan <rezafauzan593@gmail.com>
2026-03-27 14:00:10 +0700

    Feat: add GetUserProfileByUserId handler

--- internal/handlers/user_profile.handler.go
+++ internal/handlers/user_profile.handler.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"net/http"
 	"rezafauzan/koda-b6-golang/internal/dto"
+	"rezafauzan/koda-b6-golang/internal/lib"
 	"rezafauzan/koda-b6-golang/internal/services"
 	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -64,7 +66,52 @@ func (u UserProfileHandler) CreateNewUserProfile(ctx *gin.Context) {
 	})
 }
 
-func (u UserProfileHandler) UpdateUserProfileEntity(ctx *gin.Context) {
+func (u UserProfileHandler) GetUserProfile(ctx *gin.Context) {
+	authHeader := ctx.GetHeader("Authorization")
+
+	if authHeader == "" {
+		ctx.JSON(http.StatusBadRequest,
+			dto.Response{
+				Success:  false,
+				Messages: "Error missing token",
+				Results:  nil,
+			})
+		return
+	}
+
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	token = strings.TrimSpace(token)
+
+	claims, err := lib.VerifyJWT(token)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, dto.Response{
+			Success:  false,
+			Messages: err.Error(),
+			Results:  nil,
+		})
+		return
+	}
+
+	user, err := u.userProfileService.GetUserProfileByUserId(claims.User_id)
+
+	if err != nil {
+		ctx.JSON(http.StatusOK, dto.Response{
+			Success:  false,
+			Messages: err.Error(),
+			Results:  nil,
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, dto.Response{
+		Success:  true,
+		Messages: "Get User Profile Data!",
+		Results:  user,
+	})
+}
+
+func (u UserProfileHandler) UpdateUserProfile(ctx *gin.Context) {
 	var body dto.UpdateUserProfileEntityDTO
 	ctx.ShouldBind(&body)
 	updated, err := u.userProfileService.UpdateUserProfileEntity(body)
