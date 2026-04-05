e86b5e0ecce2c3c669028d31f590e890ab0d52ce
rezafauzan <rezafauzan593@gmail.com>
2026-04-05 17:25:38 +0700

    Feat: add GetProductByName and productById in product router

---
+++ internal/routers/product.routers.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	"rezafauzan/koda-b6-golang/internal/di"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewProductRouter(router *gin.Engine, container *di.Container){
+	productRoutes := router.Group("/products")
+	{
+		productRoutes.GET("", container.ProductHandler.GetAllProducts)
+		productRoutes.GET("/:productId", container.ProductHandler.GetProductById)
+	}
+}
