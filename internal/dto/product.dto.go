211240b58e569f1157571a6e624f1417e21acc85
rezafauzan <rezafauzan593@gmail.com>
2026-04-12 16:06:19 +0700

    Feat: add CreateProductRequestDTO

--- internal/dto/product.dto.go
+++ internal/dto/product.dto.go
@@ -3,12 +3,20 @@ package dto
 import "time"
 
 type ProductResponseDTO struct {
-	Id              int       `json:"id"`
-	CategoryId      int       `json:"category_id"`
-	Name            string    `json:"name"`
-	Description     string    `json:"description"`
-	Price           int       `json:"price"`
-	Stock           int       `json:"stock"`
-	CreatedAt       time.Time `json:"created_at"`
-	UpdatedAt       time.Time `json:"updated_at"`
+	Id          int       `json:"id"`
+	CategoryId  int       `json:"category_id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Price       float64       `json:"price"`
+	Stock       int       `json:"stock"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type CreateProductRequestDTO struct {
+	Name        string  `json:"name" binding:"required,min=4"`
+	CategoryId  int     `json:"category_id" binding:"required,gt=0"`
+	Description string  `json:"description" binding:"required"`
+	Price       float64 `json:"price" binding:"required,gt=0"`
+	Stock       int     `json:"stock" binding:"gte=0"`
 }
