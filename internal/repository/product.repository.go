9ff8c7e8194309cea8cba3a33a2412eef0887d7f
rezafauzan <rezafauzan593@gmail.com>
2026-04-13 16:08:07 +0700

    Feat: update create and update product repo method with new struct product model

--- internal/repository/product.repository.go
+++ internal/repository/product.repository.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"rezafauzan/koda-b6-golang/internal/models"
 	"time"
 
@@ -24,7 +26,7 @@ func NewProductRepository(db *pgxpool.Pool, rdb *redis.Client) (*ProductReposito
 }
 
 func (p ProductRepository) CreateProduct(newProduct *models.Product) (models.Product, error) {
-	sql := "INSERT INTO products (category_id, favorite_product, name, description, price, stock, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id, category_id, favorite_product, name, description, price, stock, created_at, updated_at"
+	sql := "INSERT INTO products (category_id, name, description, price, stock, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, category_id, name, description, price, stock, created_at, updated_at"
 	rows, err := p.db.Query(context.Background(), sql, newProduct.CategoryId, newProduct.Name, newProduct.Description, newProduct.Price, newProduct.Stock, time.Now(), time.Now())
 	if err != nil {
 		return models.Product{}, errors.New("Failed to create new product! : " + err.Error())
@@ -35,10 +37,38 @@ func (p ProductRepository) CreateProduct(newProduct *models.Product) (models.Pro
 		return models.Product{}, errors.New("Failed to create new product! : " + err.Error())
 	}
 
+	if p.rdb != nil {
+		cacheKey := "products"
+		err := p.rdb.Del(context.Background(), cacheKey).Err()
+		if err != nil {
+			return models.Product{}, errors.New("Redis Error : " + err.Error())
+		}
+	}
+
 	return registeredProduct, nil
 }
 
 func (p ProductRepository) GetAllProducts() ([]models.Product, error) {
+	cacheKey := "products"
+
+	if p.rdb == nil {
+		return []models.Product{}, errors.New("Redis connection not established!")
+	}
+
+	valueCache, err := p.rdb.Get(context.Background(), cacheKey).Result()
+
+	if err == nil {
+		var products []models.Product
+		if err := json.Unmarshal([]byte(valueCache), &products); err != nil {
+			return nil, err
+		}
+		return products, nil
+	}
+	fmt.Println("Cache miss")
+	if err != redis.Nil {
+		return nil, err
+	}
+
 	sql := `SELECT id, category_id, name, description, price, stock, created_at, updated_at FROM products`
 
 	rows, err := p.db.Query(context.Background(), sql)
@@ -48,16 +78,19 @@ func (p ProductRepository) GetAllProducts() ([]models.Product, error) {
 	}
 
 	products, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Product])
-
 	if err != nil {
 		return []models.Product{}, errors.New("Failed to create response get all products! : " + err.Error())
 	}
 
 	if p.rdb != nil {
-		cacheKey := "products"
-		err := p.rdb.Del(context.Background(), cacheKey).Err()
+		data, err := json.Marshal(products)
 		if err != nil {
-			return []models.Product{}, errors.New("Redis Error : " + err.Error())
+			fmt.Println("Failed to marshal cache")
+		} else {
+			err := p.rdb.Set(context.Background(), cacheKey, data, time.Hour).Err()
+			if err != nil {
+				fmt.Println("Failed to set cache")
+			}
 		}
 	}
 
@@ -110,16 +143,36 @@ func (p ProductRepository) UpdateProduct(newData models.Product) (models.Product
 		newData.Description = product.Description
 	}
 
-	sql := `UPDATE products SET category_id = $1, favorite_product = $2, name = $3, description = $4, price = $5, campaign_id = $6, stock = $7, updated_at = $8 WHERE id = $9`
-
-	_, err = p.db.Exec(context.Background(), sql, newData.CategoryId, newData.Name, newData.Description, newData.Price, newData.Stock, time.Now(), newData.Id)
-	if err != nil {
-		return models.Product{}, err
+	if newData.Price == 0 {
+		newData.Price = product.Price
 	}
 
-	updatedProduct, err := p.GetProductById(newData.Id)
+	if newData.Stock == 0 {
+		newData.Stock = product.Stock
+	}
+
+	if newData.CategoryId == 0 {
+		newData.CategoryId = product.CategoryId
+	}
+
+	sql := "UPDATE products SET category_id = $1, name = $2, description = $3, price = $4, stock = $5, updated_at = $6 WHERE id = $7 RETURNING id, category_id, name, description, price, stock, created_at, updated_at"
+
+	rows, err := p.db.Query(context.Background(), sql, newData.CategoryId, newData.Name, newData.Description, newData.Price, newData.Stock, time.Now(), newData.Id)
 	if err != nil {
-		return models.Product{}, err
+		return models.Product{}, errors.New("Failed to update product! : " + err.Error())
+	}
+
+	updatedProduct, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.Product])
+	if err != nil {
+		return models.Product{}, errors.New("Failed to update product! : " + err.Error())
+	}
+
+	if p.rdb != nil {
+		cacheKey := "products"
+		err := p.rdb.Del(context.Background(), cacheKey).Err()
+		if err != nil {
+			return models.Product{}, errors.New("Redis Error : " + err.Error())
+		}
 	}
 
 	return updatedProduct, nil
