b638f0e9e549a66ea50ae7e4198baf290c811933
rezafauzan <rezafauzan593@gmail.com>
2026-04-14 17:07:00 +0700

    Feat: optimize internal/routers/product.routers.go

--- internal/routers/product.routers.go
+++ internal/routers/product.routers.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"rezafauzan/koda-b6-golang/internal/di"
+	"rezafauzan/koda-b6-golang/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,9 +10,11 @@ import (
 func NewProductRouter(router *gin.Engine, container *di.Container){
 	productRoutes := router.Group("/products")
 	{
-		productRoutes.POST("", container.ProductHandler.CreateNewProduct)
+		productRoutes.POST("", middleware.AuthMiddleware(), middleware.RBAC("admin"), container.ProductHandler.CreateNewProduct)
 		productRoutes.GET("", container.ProductHandler.GetAllProducts)
+		productRoutes.GET("/category/:category_id", container.ProductHandler.GetProductByCategoryId)
 		productRoutes.GET("/:productId", container.ProductHandler.GetProductById)
-		productRoutes.PATCH("/:productId", container.ProductHandler.UpdateProduct)
+		productRoutes.PUT("/:productId", middleware.AuthMiddleware(), middleware.RBAC("admin"), container.ProductHandler.UpdateProduct)
+		productRoutes.DELETE("/:productId", middleware.AuthMiddleware(), middleware.RBAC("admin"), container.ProductHandler.DeleteProduct)
 	}
 }
