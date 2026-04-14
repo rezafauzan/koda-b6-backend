19bf4ee2feed87afdecb615ddefc92460a31f664
rezafauzan <rezafauzan593@gmail.com>
2026-04-14 15:36:00 +0700

    Feat: implement internal/repository/user_credential.repository.go

--- internal/repository/user_credential.repository.go
+++ internal/repository/user_credential.repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"errors"
 	"rezafauzan/koda-b6-golang/internal/models"
 	"time"
 
@@ -12,6 +11,12 @@ import (
 
 type UserCredentialRepository struct {
 	db *pgxpool.Pool
+}
+
+type UserCartRole struct {
+	UserId   int    `db:"user_id"`
+	CartId   int    `db:"cart_id"`
+	RoleName string `db:"role_name"`
 }
 
 func NewUserCredentialRepository(db *pgxpool.Pool) (*UserCredentialRepository, error) {
@@ -54,11 +59,27 @@ func (u UserCredentialRepository) UpdateUserCredential(newData models.UserCreden
 func (u UserCredentialRepository) GetUserCredentialsByEmail(email string) (models.UserCredential, error) {
 	sql := `SELECT id, user_id, email, phone, password, created_at, updated_at FROM user_credentials WHERE email = $1`
 	rows, err := u.db.Query(context.Background(), sql, email)
+	if err != nil {
+		return models.UserCredential{}, err
+	}
 	userCredentials, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.UserCredential])
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return models.UserCredential{}, errors.New("User credentials not found")
-		}
+		return models.UserCredential{}, err
 	}
 	return userCredentials, nil
+}
+
+func (u UserCredentialRepository) GetUserCartAndRoleByUserId(userId int) (UserCartRole, error) {
+	sql := `SELECT users.id AS user_id, carts.id AS cart_id, roles.role_name FROM users JOIN carts ON carts.user_id = users.id JOIN roles ON roles.id = users.role_id WHERE users.id = $1`
+	rows, err := u.db.Query(context.Background(), sql, userId)
+	if err != nil {
+		return UserCartRole{}, err
+	}
+
+	result, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[UserCartRole])
+	if err != nil {
+		return UserCartRole{}, err
+	}
+
+	return result, nil
 }
