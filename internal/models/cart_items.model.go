0ba7a97e6c6f8c77c28179e96ba9f5dc0d3e7c7e
rezafauzan <rezafauzan593@gmail.com>
2026-04-14 14:16:00 +0700

    Feat: add internal/models/cart_items.model.go

--- internal/models/cart_items.model.go
+++ internal/models/cart_items.model.go
@@ -6,8 +6,8 @@ type CartItem struct {
 	Id        int        `json:"id" db:"id"`
 	CartId    int        `json:"cart_id" db:"cart_id"`
 	ProductId int        `json:"product_id" db:"product_id"`
-	Size      string     `json:"size" db:"size"`
-	Hotice    string     `json:"hotice" db:"hotice"`
+	SizeId    int        `json:"size_id" db:"size_id"`
+	VariantId int        `json:"variant_id" db:"variant_id"`
 	Quantity  int        `json:"quantity" db:"quantity"`
 	CreatedAt time.Time  `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
