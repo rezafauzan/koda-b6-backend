b0fe88507ffcf94d18bdf2f9cefab5e925c2c516
rezafauzan <rezafauzan593@gmail.com>
2026-04-14 14:37:00 +0700

    Feat: refactor internal/repository/cart_items.repository.go

--- internal/repository/cart_items.repository.go
+++ internal/repository/cart_items.repository.go
@@ -18,9 +18,9 @@ func NewCartItemRepository(db *pgxpool.Pool) *CartItemRepository {
 }
 
 func (c *CartItemRepository) AddItem(item dto.CreateCartItemRequestDTO) (models.CartItem, error) {
-	sql := `INSERT INTO cart_items (cart_id, product_id, size, hotice, quantity, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, NOW(), NOW()) RETURNING id, cart_id, product_id, size, hotice, quantity, created_at, updated_at`
+	sql := `INSERT INTO cart_items (cart_id, product_id, size_id, variant_id, quantity, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, NOW(), NOW()) RETURNING id, cart_id, product_id, size_id, variant_id, quantity, created_at, updated_at`
 
-	rows, err := c.db.Query(context.Background(), sql, item.CartId, item.ProductId, item.Size, item.Hotice, item.Quantity)
+	rows, err := c.db.Query(context.Background(), sql, item.CartId, item.ProductId, item.SizeId, item.VariantId, item.Quantity)
 	if err != nil {
 		return models.CartItem{}, err
 	}
@@ -50,7 +50,7 @@ func (c *CartItemRepository) GetCartByUserId(userId int) (models.Cart, error) {
 }
 
 func (c *CartItemRepository) GetCartItemsByCartId(cartId int) ([]models.CartItem, error) {
-	sql := `SELECT id, cart_id, product_id, size, hotice, quantity, created_at, updated_at FROM cart_items WHERE cart_id = $1 ORDER BY created_at ASC`
+	sql := `SELECT id, cart_id, product_id, size_id, variant_id, quantity, created_at, updated_at FROM cart_items WHERE cart_id = $1 ORDER BY created_at ASC`
 
 	rows, err := c.db.Query(context.Background(), sql, cartId)
 	if err != nil {
@@ -66,10 +66,10 @@ func (c *CartItemRepository) GetCartItemsByCartId(cartId int) ([]models.CartItem
 	return cartItems, nil
 }
 
-func (c *CartItemRepository) DeleteItem(id int) (models.CartItem, error) {
-	sql := `DELETE FROM cart_items WHERE id = $1 RETURNING id, cart_id, product_id, size, hotice, quantity, created_at, updated_at`
+func (c *CartItemRepository) DeleteItem(id int, cartId int) (models.CartItem, error) {
+	sql := `DELETE FROM cart_items WHERE id = $1 AND cart_id = $2 RETURNING id, cart_id, product_id, size_id, variant_id, quantity, created_at, updated_at`
 
-	rows, err := c.db.Query(context.Background(), sql, id)
+	rows, err := c.db.Query(context.Background(), sql, id, cartId)
 	if err != nil {
 		return models.CartItem{}, err
 	}
@@ -83,7 +83,7 @@ func (c *CartItemRepository) DeleteItem(id int) (models.CartItem, error) {
 }
 
 func (c *CartItemRepository) ClearCartItem(cartId int) ([]models.CartItem, error) {
-	sql := `DELETE FROM cart_items WHERE cart_id = $1 RETURNING id, cart_id, product_id, size, hotice, quantity, created_at, updated_at`
+	sql := `DELETE FROM cart_items WHERE cart_id = $1 RETURNING id, cart_id, product_id, size_id, variant_id, quantity, created_at, updated_at`
 
 	rows, err := c.db.Query(context.Background(), sql, cartId)
 	if err != nil {
